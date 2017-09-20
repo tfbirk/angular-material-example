b186db04bebd66bd4bfc40d7b29514b6a221c345
taylor-fra-brown <taylor.fra.brown@hpe.com>
2017-09-20 16:44:47 -0500

    add avatar buttons. register google material avatars in the mdIconProvider

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   };
  
 }]);
+
+// example from: https://codepen.io/kyleledbetter/pen/gpBOdK
+app.config(function($mdIconProvider) {
+    $mdIconProvider
+      // linking to https://github.com/google/material-design-icons/tree/master/sprites/svg-sprite
+      // 
+      .iconSet('action', 'https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-action.svg', 24)
+
+      // Illustrated user icons used in the docs https://material.angularjs.org/latest/#/demo/material.components.gridList
+      .iconSet('avatars', 'https://raw.githubusercontent.com/angular/material/master/docs/app/icons/avatar-icons.svg', 24)
+      .defaultIconSet('https://raw.githubusercontent.com/google/material-design-icons/master/sprites/svg-sprite/svg-sprite-action.svg', 24);
+});
