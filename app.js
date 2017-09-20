aa4320a6ff04a0d7e6b12333dc147ea8a19a6852
taylor-fra-brown <taylor.fra.brown@hpe.com>
2017-09-20 15:15:42 -0500

    initial commit based on codepen example

---
+++ app.js
@@ -0,0 +1,10 @@
+// example from: https://codepen.io/marcysutton/pen/OPbpKm/
+
+var app = angular.module('StarterApp', ['ngMaterial']);
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+ 
+}]);
